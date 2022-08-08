658d4ba0b52619eb784a7d60d529556c77aefdce
jojo <jojo@LAPTOP-APBPR5ON>
2022-08-08 18:46:52 +0300

    Add project files.

---
+++ Logic/XoGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Logic
+{
+    public class XoGame
+    {
+        bool?[,] gameXo = new bool?[4,3];
+        string winnerName = "";
+        int count = 0;
+
+        public String SendTurn(bool xTurnBtn, int row, int col)
+        {
+            gameXo[row, col] = xTurnBtn;
+            count++;
+            return CheckWins();   
+        }
+
+        private string CheckWins()
+        {
+            //at the end of a turn, use this is method
+            for (int i = 0; i < gameXo.GetLength(0) && count > 4 ; i++)
+            {
+                for (int j = 0; j < gameXo.GetLength(1); j++)
+                {  //check row
+                    if (gameXo[i, j] == null ) continue;
+                    else if (j==0 &&(gameXo[i, j] == gameXo[i, j + 1]) && gameXo[i, j] == gameXo[i, j + 2])
+                       return winnerName = CheckNameWinner(i, j);
+                    //check col
+                    else if (i== 0 && (gameXo[i, j] == gameXo[i + 1, j]) && gameXo[i, j] == gameXo[i + 2, j])
+                       return winnerName = CheckNameWinner(i, j);
+                    //אלכסון ראשי 
+                    else if (i == j && i == 0 && gameXo[i, j] == gameXo[i + 1, j + 1] && gameXo[i, j] == gameXo[i + 2, j + 2])
+                       return winnerName = CheckNameWinner(i, j);
+                    //אלכסון משני
+                    else if (i==0 && i + j == gameXo.GetLength(0) - 2 && gameXo[i, j] == gameXo[i + 1, j - 1] && gameXo[i, j] == gameXo[i + 2, j - 2])
+                       return winnerName = CheckNameWinner(i, j);
+                    else if (count == 9 && i == 2 && j == 2 && winnerName != "X" && winnerName != "O") return "Tie";
+
+                }
+            }
+            return null;
+        }
+
+        private string CheckNameWinner(int i, int j)
+        {
+            if (gameXo[i, j] == true) return "X";
+            return "O";
+        }
+
+      
+    }
+}
---
+++ WpfGameXo/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Logic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfGameXo
+{
+    public partial class MainWindow : Window
+    {
+        bool xTurnBtn;
+        XoGame xoGame = new XoGame();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BuildGame();
+        }
+
+        private void BuildGame()
+        {
+            xTurnBtn = false;
+            xoGame = new XoGame();
+            Grid grid = new Grid();
+            //grid.Width = 470;
+            //grid.Height = 470;
+            for (int i = 0; i < 4; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var btnStartGame = new Button();
+                    if (i == 3)
+                    {
+                        Grid.SetRow(btnStartGame, 3);
+                        Grid.SetColumnSpan(btnStartGame, 3);
+                       
+                        grid.Children.Add(btnStartGame);
+                        btnStartGame.Click += NewGame;
+                        btnStartGame.Style = (Style)App.Current.Resources["btnStartGame"];
+                    }
+                    if(i != 3)
+                    {
+                        var btn = new Button();
+                        btn.Style = (Style)App.Current.Resources["defTurn"];
+                       btn.Click += GameXoClick;
+                        Grid.SetRow(btn, i);
+                        Grid.SetColumn(btn, j);
+                        grid.Children.Add(btn);
+                    }
+                }
+            }
+            Content = grid;
+        }
+
+        private void NewGame(object sender, RoutedEventArgs e)
+        {
+            BuildGame();
+        }
+
+        private void GameXoClick(object sender, RoutedEventArgs e)
+        {
+            xTurnBtn = !xTurnBtn;
+
+            if (sender is Button btn)
+            {
+                var row = Grid.GetRow(btn);
+                var col = Grid.GetColumn(btn);
+                var res = xoGame.SendTurn(xTurnBtn, row, col);
+
+                btn.Style = (Style)App.Current.Resources[xTurnBtn ? "xTurnBtn" : "oTurnBtn"];
+                btn.Click -= GameXoClick;
+
+                if (res != null)
+                {
+                    MessageBox.Show($"The winner is {res}");
+                    BuildGame();
+                }
+
+            }
+        }
+    }
+}
